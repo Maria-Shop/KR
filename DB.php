6ce1cfe1d946ecc0266f755e51963fe2d14064dd
Maria-Shop <maria.shop.1995@gmail.com>
2016-06-04 13:44:32 +0300

    Update DB.php

--- DB.php
+++ DB.php
@@ -1,32 +1,26 @@
 <?php
-
-
 	class DB
 	{
 		private $conn;
-	
-	
-	
+
 		function DB()
 		{
 			try
 			{
 				$this->conn  = new PDO ('mysql:host=localhost; dbname=Shop', 'root','');
-				
 				//echo "Connection ok ";
-				
 			}
-
 
 			catch(PDOException $e)
 			{
 				echo "Connection failed".$e->getMessage();
 			}
 		}
-function getConn()
-	{
-		return $this->conn;
-	}
+		
+	function getConn()
+		{
+			return $this->conn;
+		}
 
 	}
 ?>
